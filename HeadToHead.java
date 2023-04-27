db07e078fba5ff0f1c2e41c4242e778b6ef74bba
AlexW18 <131005996+AlexW18@users.noreply.github.com>
2023-04-27 11:23:25 -0400

    Update HeadToHead.java

--- HeadToHead.java
+++ HeadToHead.java
@@ -6,7 +6,7 @@ public class HeadToHead {
 		
 		ArrayList<Team> teams = new ArrayList<Team>();
 		
-		String winner = "";
+		public Team winner = "";
 		
 		if (teams.get(0).getTeamWorth() > teams.get(1).getTeamWorth() ) {
 			winner = teams.get(0).getName();
