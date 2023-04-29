3d0f48ab899c0a9463f52548b11fe1b79bc522e6
AlexW18 <131005996+AlexW18@users.noreply.github.com>
2023-04-29 13:17:03 -0400

    Update HeadToHead.java
    A possible better option for the HeadToHead Class is commented below the actual class

--- HeadToHead.java
+++ HeadToHead.java
@@ -16,8 +16,25 @@ public class HeadToHead {
 			teams.get(1).addWin();
 			teams.get(0).addLoss();
 		}
+		return winner;
+	}
+}
+
+/*
+private Team winner = null;	
+	
+	public Team getMatchup(Team team1, Team team2) {
+		
+		if(team1.getTeamWorth() > team2.getTeamWorth()) {
+			winner = team1;
+			team1.addWin();
+			team2.addLoss();
+		} else {
+			winner = team2;
+			team2.addWin();
+			team1.addLoss();
+		}
 		
 		return winner;
 	}
-	
-}
+	*/
