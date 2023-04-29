d070232a263bede5b41d370bc9ddcd7eddb80a0e
AlexW18 <131005996+AlexW18@users.noreply.github.com>
2023-04-29 14:19:39 -0400

    Update HeadToHead.java

--- HeadToHead.java
+++ HeadToHead.java
@@ -2,26 +2,7 @@ import java.util.ArrayList;
 
 public class HeadToHead {
 	
-	ArrayList<Team> teams = new ArrayList<Team>();
-	private Team winner = null;
-	
-	public Team getMatchup() {
-		
-		if(teams.get(0).getTeamWorth()>teams.get(1).getTeamWorth()) {
-			winner = teams.get(0);
-			teams.get(0).addWin();
-			teams.get(1).addLoss();
-		} else {
-			winner = teams.get(1);
-			teams.get(1).addWin();
-			teams.get(0).addLoss();
-		}
-		return winner;
-	}
-}
-
-/*
-private Team winner = null;	
+	private Team winner = null;	
 	
 	public Team getMatchup(Team team1, Team team2) {
 		
@@ -37,4 +18,4 @@ private Team winner = null;
 		
 		return winner;
 	}
-	*/
+}
